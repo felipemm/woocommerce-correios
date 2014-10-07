c786eac2dc7f6392bc6de011334ee9f4bdf35aa5
Felipe Matos Moreira <felipematosmoreira@gmail.com>
2014-10-07 15:09:36 -0300

    added v1.3

--- woocommerce-correios.php
+++ woocommerce-correios.php
@@ -3,12 +3,11 @@
 Plugin Name: WooCommerce Correios
 Plugin URI: http://felipematos.com/loja
 Description: Adiciona entrega por correios
-Version: 1.0
+Version: 1.3
 Author: Felipe Matos <chucky_ath@yahoo.com.br>
 Author URI: http://felipematos.com
-License: GPLv2
 Requires at least: 3.0
-Tested up to: 3.3.1
+Tested up to: 3.3.4
 */
 
 //hook to include the payment gateway function
@@ -16,6 +15,9 @@ add_action('plugins_loaded', 'shipping_correios', 0);
 
 //hook function
 function shipping_correios(){
+
+	require_once('correios.php');
+
     class correios extends WC_Shipping_Method {
 
         function __construct() { 
@@ -43,7 +45,11 @@ function shipping_correios(){
             $this->enable_sedex = $this->settings['enable_sedex']; 
             $this->enable_sedex_cobrar = $this->settings['enable_sedex_cobrar']; 
             $this->enable_sedex10 = $this->settings['enable_sedex10']; 
+            $this->enable_sedex_hoje = $this->settings['enable_sedex_hoje']; 
             $this->enable_esedex = $this->settings['enable_esedex']; 
+            $this->valor_declarado = $this->settings['valor_declarado']; 
+            $this->mao_propria = $this->settings['mao_propria']; 
+            $this->aviso_recebimento = $this->settings['aviso_recebimento']; 
             $this->cod_empresa = $this->settings['cod_empresa']; 
             $this->senha = $this->settings['senha']; 
             
@@ -60,69 +66,93 @@ function shipping_correios(){
         function init_form_fields() {
             $this->form_fields = array(
                 'enabled' => array(
-                    'title'  => __( 'Enable', 'woothemes' ), 
+                    'title'  => __( 'Habilita/Desabilita', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable local pickup', 'woothemes' ), 
+                    'label' => __( 'Habilita envio por Correios', 'woothemes' ), 
                     'default' => 'yes'
                 ),
                 'title' => array(
-                    'title' => __( 'Title', 'woothemes' ),
+                    'title' => __( 'Título', 'woothemes' ),
                     'type' => 'text',
-                    'description' => __( 'This controls the title which the user sees during checkout.', 'woothemes' ),
+                    'description' => __( 'Título a ser exibido durante o checkout.', 'woothemes' ),
                     'default' => __( 'Correios', 'woothemes' )
                 ),
                 'postalcode' => array(
-                    'title' => __( 'Sender Postal Code', 'woothemes' ),
+                    'title' => __( 'CEP de Origem', 'woothemes' ),
                     'type' => 'text',
-                    'description' => __( 'This controls the title which the user sees during checkout.', 'woothemes' ),
-                    'default' => __( '05713520', 'woothemes' )
+                    'description' => __( 'CEP onde se encontra o produto para calcular o frete.', 'woothemes' ),
+                    'default' => __( '00000000', 'woothemes' )
                 ),
                 'debug' => array(
                     'title' => __( 'Debug', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable debugging <code>/woocommerce/logs/correios.log</code>', 'woothemes' ), 
+                    'label' => __( 'Habilita escrita de log <code>/woocommerce/logs/correios.log</code>.', 'woothemes' ), 
                     'default' => 'yes'
                 ),
                 'enable_pac' => array(
                     'title' => __( 'PAC', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable PAC shipping method', 'woothemes' ), 
+                    'label' => __( 'Habilita envio por PAC.', 'woothemes' ), 
                     'default' => 'yes'
                 ),
                 'enable_sedex' => array(
                     'title' => __( 'SEDEX', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable SEDEX shipping method', 'woothemes' ), 
+                    'label' => __( 'Habilita envio por SEDEX.', 'woothemes' ), 
                     'default' => 'yes'
                 ),
                 'enable_sedex_cobrar' => array(
                     'title' => __( 'SEDEX a Cobrar', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable SEDEX a cobrar shipping method', 'woothemes' ), 
-                    'default' => 'yes'
+                    'label' => __( 'Habilita envio por SEDEX à cobrar (necessita declaração de valor).', 'woothemes' ), 
+                    'default' => 'no'
                 ),
                 'enable_sedex10' => array(
                     'title' => __( 'SEDEX 10', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable SEDEX 10 shipping method', 'woothemes' ), 
-                    'default' => 'yes'
+                    'label' => __( 'Habilita envio por SEDEX 10.', 'woothemes' ), 
+                    'default' => 'no'
+                ),
+                'enable_sedex_hoje' => array(
+                    'title' => __( 'SEDEX Hoje', 'woothemes' ), 
+                    'type' => 'checkbox', 
+                    'label' => __( 'Habilita envio por SEDEX Hoje.', 'woothemes' ), 
+                    'default' => 'no'
                 ),
                 'enable_esedex' => array(
                     'title' => __( 'e-Sedex', 'woothemes' ), 
                     'type' => 'checkbox', 
-                    'label' => __( 'Enable e-Sedex (with contract) shipping method', 'woothemes' ), 
-                    'default' => 'yes'
+                    'label' => __( 'Habilita envio por e-SEDEX (necessita contrato com correios).', 'woothemes' ), 
+                    'default' => 'no'
+                ),
+                'valor_declarado' => array(
+                    'title' => __( 'Declarar Valor?', 'woothemes' ), 
+                    'type' => 'checkbox', 
+                    'label' => __( 'Utiliza a opção de valor declarado no pacote para os correios. Necessário para Sedex à cobrar.', 'woothemes' ), 
+                    'default' => 'no'
+                ),
+                'mao_propria' => array(
+                    'title' => __( 'Mão própria?', 'woothemes' ), 
+                    'type' => 'checkbox', 
+                    'label' => __( 'Utiliza a opção de mão própria no pacote para os correios.', 'woothemes' ), 
+                    'default' => 'no'
+                ),
+                'aviso_recebimento' => array(
+                    'title' => __( 'Aviso de Recebimento?', 'woothemes' ), 
+                    'type' => 'checkbox', 
+                    'label' => __( 'Utiliza a opção de aviso de recebimento no pacote para os correios.', 'woothemes' ), 
+                    'default' => 'no'
                 ),
                 'cod_empresa' => array(
                     'title' => __( 'Código Administrativo', 'woothemes' ), 
                     'type' => 'text', 
-                    'description' => __( 'The administrative code registered in Correios (necessary for eSedex)', 'woothemes' ), 
+                    'description' => __( 'Código informado pelos correios ao firmar o contrato (necessário para e-SEDEX).', 'woothemes' ), 
                     'default' => ''
                 ),
                 'senha' => array(
                     'title' => __( 'Senha', 'woothemes' ), 
                     'type' => 'text', 
-                    'description' => __( 'Password to access the services for you contract (necessary for eSedex)', 'woothemes' ), 
+                    'description' => __( 'Senha de acesso do seu contrato nos correios (necessário para e-SEDEX).', 'woothemes' ), 
                     'default' => ''
                 )
             );
@@ -149,27 +179,7 @@ function shipping_correios(){
             return apply_filters( 'woocommerce_shipping_' . $this->id . '_is_available', true );
         }
         
-        function calculaFrete($cod_servico, $cep_origem, $cep_destino, $peso, $altura='2', $largura='11', $comprimento='16', $valor_declarado='0.50', $codigo_empresa='',$senha=''){
-            #OFICINADANET###############################
-            # Código dos Serviços dos Correios
-            # 41106 PAC sem contrato
-            # 40010 SEDEX sem contrato
-            # 40045 SEDEX a Cobrar, sem contrato
-            # 40215 SEDEX 10, sem contrato
-            # 40290 SEDEX HOJE
-            ############################################
-            global $woocommerce;
-            
-            $correios = "http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=".$codigo_empresa."&sDsSenha=".$senha."&sCepOrigem=".$cep_origem."&sCepDestino=".$cep_destino."&nVlPeso=".$peso."&nCdFormato=1&nVlComprimento=".$comprimento."&nVlAltura=".$altura."&nVlLargura=".$largura."&sCdMaoPropria=n&nVlValorDeclarado=".$valor_declarado."&sCdAvisoRecebimento=n&nCdServico=".$cod_servico."&nVlDiametro=0&StrRetorno=xml";
-            if ($this->debug=='yes') $this->log->add( 'correios', "URL: ".$correios);
-            $xml = simplexml_load_file($correios);
-            if($xml->cServico->Erro == '0')
-                return $xml->cServico->Valor;
-            else
-                return false;
-            //return 99;
-        }
-            
+		
         function calculate_shipping(){
             global $woocommerce;
             $customer = $woocommerce->customer;  
@@ -177,60 +187,75 @@ function shipping_correios(){
             $length = 0;
             $width = 0;
             $height = 0;
-            $peso_cubico = 0;
+			$weight = 0;
+            $valor_total = 0;
 
             // Shipping per item
             foreach ($woocommerce->cart->get_cart() as $item_id => $values) {
                 $_product = $values['data'];
                 if ($values['quantity']>0) { //&& $_product->needs_shipping()) {
-                    if($_product->has_dimensions()){
-                        if ($this->debug=='yes') $this->log->add( 'correios', "Produto: ".$_product->get_sku());
-                        if ($this->debug=='yes') $this->log->add( 'correios', "Dimensões: ".$_product->get_dimensions());
-                        $dimensions = explode(' × ',$_product->get_dimensions());
-                        $length += $dimensions[0];
-                        $width += $dimensions[1];
-                        $height += str_replace(' '.get_option('woocommerce_dimension_unit'),'',$dimensions[2]);
-                        $peso_cubico = $peso_cubico + (($length * $width * $height)/1000);
-                        $valor_total += $_product->get_price();
-                    }
+					for($i=0;$i<$values['quantity'];$i++){
+						if($_product->has_dimensions()){
+							//if ($this->debug=='yes') $this->log->add( 'correios', "Produto: ".$_product->get_sku());
+							//if ($this->debug=='yes') $this->log->add( 'correios', "Dimensões: ".$_product->get_dimensions());
+							//if ($this->debug=='yes') $this->log->add( 'correios', "Peso: ".$_product->get_weight());
+							$dimensions = explode(' × ',$_product->get_dimensions());
+							$length += $dimensions[0];
+							$width += $dimensions[1];
+							$height += str_replace(' '.get_option('woocommerce_dimension_unit'),'',$dimensions[2]);
+							$weight += $_product->get_weight();
+							$valor_total += $_product->get_price();
+						}
+					}
                 }
             }
-            if ($length < 16){
-                if ($this->debug=='yes') $this->log->add( 'correios', "Comprimento não pode ser inferior a 16 cm. Ajustando Comprimento...");
-                $length = 16;
-            }
-            
-            if ($this->debug=='yes') $this->log->add( 'correios', "Peso cúbico: ".$peso_cubico);
-            if ($this->debug=='yes') $this->log->add( 'correios', "Altura: ".$height);
-            if ($this->debug=='yes') $this->log->add( 'correios', "Largura: ".$width);
-            if ($this->debug=='yes') $this->log->add( 'correios', "Comprimento: ".$length);
-            
-            // Register the rate
-            if($this->enable_pac=='yes') {
-                $cost_pac = number_format($this->calculaFrete('41106',$this->postalcode,$woocommerce->customer->get_postcode(),$peso_cubico,$height ,$width,$length,$valor_total),2,'.','');
-                if ($this->debug=='yes') $this->log->add( 'correios', "Valor PAC: ".$cost_pac);
-                $this->add_rate(array('id'=> 'pac','label'=> 'PAC','cost'=> $cost_pac,'calc_tax'=>'per_order'));
-            }
-            if($this->enable_sedex=='yes') {
-                $cost_sedex = number_format($this->calculaFrete('40010',$this->postalcode,$woocommerce->customer->get_postcode(),$peso_cubico,$height ,$width,$length,$valor_total),2,'.','');
-                if ($this->debug=='yes') $this->log->add( 'correios', "Valor SEDEX: ".$cost_sedex);
-                $this->add_rate(array('id'=> 'sedex','label'=> 'SEDEX','cost'=> $cost_sedex,'calc_tax'=>'per_order'));
-            }
-            if($this->enable_sedex_cobrar=='yes') {
-                $cost_sedex_cobrar = number_format($this->calculaFrete('40045',$this->postalcode,$woocommerce->customer->get_postcode(),$peso_cubico,$height ,$width,$length,$valor_total),2,'.','');
-                if ($this->debug=='yes') $this->log->add( 'correios', "Valor SEDEX a Cobrar: ".$cost_sedex_cobrar);
-                $this->add_rate(array('id'=> 'sedexcobrar','label'=> 'SEDEX a cobrar','cost'=> $cost_sedex_cobrar,'calc_tax'=>'per_order'));
-            }
-            if($this->enable_sedex10=='yes') {
-                $cost_sedex10 = number_format($this->calculaFrete('40215',$this->postalcode,$woocommerce->customer->get_postcode(),$peso_cubico,$height ,$width,$length,$valor_total),2,'.','');
-                if ($this->debug=='yes') $this->log->add( 'correios', "Valor SEDEX 10: ".$cost_sedex10);
-                $this->add_rate(array('id'=> 'sedex10','label'=> 'SEDEX 10','cost'=> $cost_sedex10,'calc_tax'=>'per_order'));
-            }
-            if($this->enable_esedex=='yes' && $this->cod_empresa!='' && $this->senha!='') {
-                $cost_esedex = number_format($this->calculaFrete('81019',$this->postalcode,$woocommerce->customer->get_postcode(),$peso_cubico,$height ,$width,$length,$valor_total),2,'.','');
-                if ($this->debug=='yes') $this->log->add( 'correios', "Valor e-Sedex: ".$cost_esedex);
-                $this->add_rate(array('id'=> 'esedex','label'=> 'e-Sedex','cost'=> $cost_esedex,'calc_tax'=>'per_order'));
-            }
+			
+			$maopropria=($this->mao_propria=='yes'?true:false);
+			$valordeclarado=($this->valor_declarado=='yes'?true:false);
+			$avisorecebimento=($this->aviso_recebimento=='yes'?true:false);
+			
+            if ($this->debug=='yes') $this->log->add( 'correios', "dimensões: ".$length.'x'.$height.'x'.$width.' - peso: '.$weight.' - valor: '.$valor_total.' - CEP: '.$woocommerce->customer->get_postcode());
+            if ($this->debug=='yes') $this->log->add( 'correios', "mão própria: ".$maopropria." - valor declarado: ".$valordeclarado." - aviso recebimento: ".$avisorecebimento);
+
+			$correios = new frete_correios($this->postalcode, $maopropria, $valordeclarado, $avisorecebimento, $this->cod_empresa, $this->senha);
+			$frete = $correios->calculaFrete($woocommerce->customer->get_postcode(), $weight, $length, $height, $width, $valor_total);
+			
+			foreach($frete as $key => $value){
+				switch($key){
+					case 'pac':
+						if($this->enable_pac=='yes' && $value['valor'] > 0 && $value['erro'] == '(0)'){
+							$this->add_rate(array('id'=> 'pac','label'=> 'PAC','cost'=> $value['valor'],'calc_tax'=>'per_order'));
+						}
+						break;
+					case 'sedex':
+						if($this->enable_sedex=='yes' && $value['valor'] > 0 && $value['erro'] == '(0)'){
+							$this->add_rate(array('id'=> 'sedex','label'=> 'SEDEX','cost'=> $value['valor'],'calc_tax'=>'per_order'));
+						}
+						break;
+					case 'sedex_10':
+						if($this->enable_sedex10=='yes' && $value['valor'] > 0 && $value['erro'] == '(0)'){
+							$this->add_rate(array('id'=> 'sedex10','label'=> 'SEDEX10','cost'=> $value['valor'],'calc_tax'=>'per_order'));
+						}
+						break;
+					case 'sedex_hoje':
+						if($this->enable_sedex_hoje=='yes' && $value['valor'] > 0 && $value['erro'] == '(0)'){
+							$this->add_rate(array('id'=> 'sedexhoje','label'=> 'SEDEX Hoje','cost'=> $value['valor'],'calc_tax'=>'per_order'));
+						}
+						break;
+					case 'sedex_cobrar':
+						if($this->enable_sedex_cobrar=='yes' && $value['valor'] > 0 && $value['erro'] == '(0)'){
+							$this->add_rate(array('id'=> 'sedexcobrar','label'=> 'SEDEX à Cobrar','cost'=> $value['valor'],'calc_tax'=>'per_order'));
+						}
+						break;
+					case 'esedex':
+						if($this->enable_esedex=='yes' && $value['valor'] > 0 && $value['erro'] == '(0)'){
+							$this->add_rate(array('id'=> 'esedex','label'=> 'e-SEDEX','cost'=> $value['valor'],'calc_tax'=>'per_order'));
+						}
+						break;
+				}
+				
+				if ($this->debug=='yes') $this->log->add( 'correios', $key .' = R$ '. $value['valor'] .' | '. $value['erro']);
+			}
         }
     }
   
@@ -240,3 +265,4 @@ function shipping_correios(){
 
     add_filter('woocommerce_shipping_methods', 'add_correios_method' );
 }
+?>
